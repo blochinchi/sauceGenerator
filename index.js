7e34d6af146570712e35841b0104437bfbcddebf
blochinchi <59876671+blochinchi@users.noreply.github.com>
2020-09-03 11:50:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-var sauceNumber = Math.random();
-sauceNumber = sauceNumber * 327000;
-if (sauceNumber < 1) {
-  Math.roof(sauceNumber);
-} else {
-  sauceNumber = Math.floor(sauceNumber);
-}
-console.log(sauceNumber);
+var coinToss = Math.random();
+coinToss = coinToss*2;
+coinToss = Math.floor(coinToss)
+coinToss = coinToss+1
+console.log(coinToss);
+
+
+
+
 function buttonFunction(){
-document.getElementById("linkForSauce").href = "https://nhentai.net/g/"+sauceNumber;
+  if(coinToss == 1){
+    var doujinSauceNumber = Math.random();
+    doujinSauceNumber = doujinSauceNumber * 327000;
+    if (doujinSauceNumber < 1) {
+      Math.roof(doujinSauceNumber);
+    }
+    else {
+      doujinSauceNumber = Math.floor(doujinSauceNumber);
+    }
+    console.log(doujinSauceNumber);
+    document.getElementById("linkForSauce").href = "https://nhentai.net/g/"+doujinSauceNumber;
+  }
+
+  else if (coinToss == 2){
+    var hanimeSauceNumber = Math.random();
+    hanimeSauceNumber = hanimeSauceNumber * 2000000000000;
+    if (hanimeSauceNumber < 1) {
+      Math.roof(hanimeSauceNumber);
+    }
+    else {
+      hanimeSauceNumber = Math.floor(hanimeSauceNumber);
+    }
+    console.log(hanimeSauceNumber);
+    document.getElementById("linkForSauce").href = "https://hanime.tv/browse/random?r="+hanimeSauceNumber;
+  }
 }
